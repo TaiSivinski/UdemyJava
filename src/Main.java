251ea57d1bcd08127b49be523af942bdb906015b
Tainara Bettoni Oliveira <tbettoni@emeal.nttdata.com>
2022-09-01 11:24:06 -0300

    firsto commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Locale;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        int x = 10;
+        int y = 30;
+
+        System.out.println("Resultado é " + x + y);
+
+
+    }
+
+}
